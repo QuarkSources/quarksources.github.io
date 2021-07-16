b33d53edb170e211d8cf144464b0d7910a53ec06
Quark Sources <76851962+QuarkSources-bot@users.noreply.github.com>
2021-07-15 21:23:45 -0500

    Reset commit history.

---
+++ sourceUtil.py
@@ -0,0 +1,226 @@
+
+from pathlib import Path
+import plistlib
+import requests
+import re
+from datetime import datetime
+from packaging import version
+from tempfile import TemporaryDirectory
+from zipfile import ZipFile
+import json
+
+def compare_versions(v1: str, v2: str) -> bool:
+    """
+    Returns true if v1 is 'older than' v2
+    """
+    return version.parse(v1) < version.parse(v2)
+
+class AltSourceManager:
+    def __init__(self, filepath, sources_data: list, alternate_data: dict = None, prettify: bool = True):
+        """
+        filepath: The location of the source to be parsed. Accepts either Path object, or string path.
+        """
+        self.src_data, self.alt_data, self.prettify = sources_data, alternate_data, prettify
+
+        if not isinstance(filepath, Path):
+            filepath = Path(filepath)
+        self.path = filepath
+        with open(self.path, "r", encoding="utf-8") as fp:
+            self.src = json.load(fp)
+
+    def create(self):
+        raise NotImplementedError()
+        print("x apps added, x news added.")
+
+    def update(self):
+        print(self.src["name"])
+        existingAppIDs = [app["bundleIdentifier"] for app in self.src["apps"]]
+        existingNewsIDs = [app["identifier"] for app in self.src["news"]]
+        updatedAppsCount = addedAppsCount = addedNewsCount = 0
+
+        for data in self.src_data:
+            try:
+                parser = data["parser"](**data["kwargs"])
+            except (json.JSONDecodeError, requests.RequestException) as err:
+                print(f"Unable to process {data.get('ids')}.")
+                print(f"{type(err).__name__}: {str(err)}")
+                continue
+
+            # perform different actions depending on the type of file being parsed
+            if isinstance(parser, AltSourceParser):
+                apps = parser.parse_apps(None if data.get("getAllApps") else data.get("ids"))
+                for app in apps:
+                    bundleID = app["bundleIdentifier"]
+                    if bundleID in existingAppIDs:
+                        if version.parse(app["version"]) > version.parse(self.src["apps"][existingAppIDs.index(bundleID)]["version"]):
+                            updatedAppsCount += 1
+                        self.src["apps"][existingAppIDs.index(bundleID)] = app # note that this actually updates the app regardless of whether the version is newer
+                    else:
+                        addedAppsCount += 1
+                        self.src["apps"].append(app)
+
+                if not data.get("ignoreNews"):
+                    news = parser.parse_news(None if data.get("getAllNews") else data.get("ids"))
+                    for article in news:
+                        newsID = article["identifier"]
+                        if newsID in existingNewsIDs:
+                            self.src["news"][existingNewsIDs.index(newsID)] = article # overwrite existing news article
+                        else:
+                            addedNewsCount += 1
+                            primarySource["news"].append(article)
+
+            elif isinstance(parser, GithubParser):
+                app = self.src["apps"][existingAppIDs.index(data["ids"][0])]
+                if version.parse(app["absoluteVersion"] if app.get("absoluteVersion") else app["version"]) < version.parse(parser.version):
+                    metadata = parser.get_asset_metadata()
+                    if metadata["bundleID"] != data["ids"][0]:
+                        print(app["name"] + " BundleID has changed to " + metadata["bundleID"] + "\nApp not updated.")
+                        continue
+                    app["absoluteVersion"] = parser.version
+                    app["versionDate"] = parser.versionDate
+                    app["versionDescription"] = parser.versionDescription
+                    for (k,v) in metadata.items():
+                        app[k] = v
+                    self.src["apps"][existingAppIDs.index(data["ids"][0])] = app
+                    updatedAppsCount += 1
+            else:
+                raise NotImplementedError()
+            # end of for loop
+
+        if self.alt_data is not None:
+            self.alter_app_info()
+
+        with open(self.path, "w", encoding="utf-8") as fp:
+            json.dump(self.src, fp, indent = 2 if self.prettify else None)
+            fp.write("\n") # add missing newline to EOF
+        print(f"{updatedAppsCount} app(s) updated.\n{addedAppsCount} app(s) added, {addedNewsCount} news article(s) added.")
+
+    def alter_app_info(self) -> list:
+        apps = self.src["apps"]
+        for i in range(len(apps)):
+            bundleID = apps[i]["bundleIdentifier"]
+            if bundleID in self.alt_data.keys():
+                for key in self.alt_data[bundleID].keys():
+                    apps[i][key] = self.alt_data[bundleID][key]
+        self.src["apps"] = apps
+
+class AltSourceParser:
+    def __init__(self, filepath):
+        """
+        filepath: The location of the source to be parsed. Accepts either Path object, string path to local object, or url.
+        """
+        if isinstance(filepath, Path) and filepath.exists():
+            with open(filepath, "r", encoding="utf-8") as fp:
+                self.src = json.load(fp)
+        elif isinstance(filepath, str) and filepath.startswith("http"):
+            self.src = requests.get(filepath).json()
+        else:
+            try:
+                path = Path(filepath)
+                with open(path, "r", encoding="utf-8") as fp:
+                    self.src = json.load(fp)
+            except Exception as err:
+                raise TypeError("Filepath must be a path-like object or a url.")
+
+        if not self.valid_source():
+            raise Exception("Invalid source formatting.")
+
+    def parse_apps(self, ids: list = None) -> list:
+        apps = list()
+        for app in self.src["apps"]:
+            if self.valid_app(app):
+                if ids is None:
+                    apps.append(app)
+                elif app["bundleIdentifier"] in ids:
+                    apps.append(app)
+        return apps
+
+    def parse_news(self, ids: list = None) -> list:
+        news = list()
+        for article in self.src["news"]:
+            if self.valid_news(article):
+                if ids is None:
+                    news.append(article)
+                elif article["identifier"] in ids:
+                    news.append(article)
+        return news
+
+    def valid_app(self, app: dict) -> bool:
+        requiredKeys = ["name", "bundleIdentifier", "developerName", "version", "versionDate", "downloadURL", "localizedDescription", "iconURL", "size"]
+        for key in requiredKeys:
+            if key not in app.keys():
+                return False
+        return True
+
+    def valid_news(self, article: dict) -> bool:
+        requiredKeys = ["title", "identifier", "caption", "date"]
+        for key in requiredKeys:
+            if key not in article.keys():
+                return False
+        return True
+
+    def valid_source(self) -> bool:
+        requiredKeys = ["name", "identifier", "apps", "news"]
+        for key in requiredKeys:
+            if key not in self.src.keys():
+                return False
+        return True
+
+class GithubParser:
+    def __init__(self, url: str = None, repo_author: str = None, repo_name: str = None, ver_parse = lambda x: x.lstrip("v"), include_pre: bool = False, prefer_date: bool = False):
+        """
+        Supply either the api url, or the repo_author and repo_name.
+        Include a lambda function for ver_parse if needed. ex: (lambda x: x.replace("-r", "."))
+        """
+        if url is not None:
+            releases = requests.get(url).json()
+        else:
+            releases = requests.get("https://api.github.com/repos/{0}/{1}/releases".format(repo_author, repo_name)).json()
+        if isinstance(releases, dict) and releases.get("message") == "Not Found":
+            raise Exception("Github Repo not found.")
+        if not include_pre:
+            releases = list(filter(lambda x: x["prerelease"] != True, releases)) # filter out prereleases
+
+        # alter the github release tags to match altstore version tags
+        releases = [{k: ver_parse(v) if k == "tag_name" else v for (k, v) in x.items()} for x in releases]
+
+        if prefer_date:
+            self.data = sorted(releases, key=lambda x: datetime.strptime(x["published_at"], "%Y-%m-%dT%H:%M:%S%z"))[-1] # only grab the most recent release
+        else:
+            self.data = sorted(releases, key=lambda x: version.parse(x["tag_name"]))[-1] # only grab the release with the highest version
+
+    @property
+    def version(self) -> str:
+        return self.data["tag_name"]
+
+    @property
+    def versionDate(self) -> str:
+        return self.data["published_at"]
+
+    @property
+    def versionDescription(self) -> str:
+        return "# " + self.data["name"] + "\n\n" + self.data["body"]
+
+    def get_asset_metadata(self, asset_name: str = None) -> dict:
+        """
+        Returns a dictionary containing the downloadURL, size, bundleID, version
+        """
+        name = asset_name + ".ipa" if asset_name is not None else ".ipa"
+        download_url = next(x for x in self.data["assets"] if x["name"].endswith(name))["browser_download_url"]
+        with TemporaryDirectory() as td:
+            tempdir = Path(str(td))
+            r = requests.get(download_url)
+            with open(tempdir / name, "wb") as file:
+                file.write(r.content)
+            with ZipFile(tempdir / name, "r") as ipa:
+                ipa.extractall(path=tempdir)
+            with open(list(tempdir.rglob("Info.plist"))[0], "rb") as fp:
+                plist = plistlib.load(fp)
+
+            metadata = {
+                "downloadURL": download_url,
+                "size": (tempdir / name).stat().st_size,
+                "bundleID": plist["CFBundleIdentifier"],
+                "version": plist["CFBundleShortVersionString"]
+            }
+        return metadata
---
+++ update.py
@@ -0,0 +1,169 @@
+from sourceUtil import AltSourceManager, AltSourceParser, GithubParser
+
+sourcesData = [
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "https://altstore.oatmealdome.me"},
+        "ids": ["me.oatmealdome.dolphinios-njb", "me.oatmealdome.DolphiniOS-njb-patreon-beta"]
+    },
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "quarksource.json"},
+        "ids": ["science.xnu.undecimus", "net.hollr.Provenance4", "com.libretro.dist.ios.RetroArch", "com.louisanslow.record", "org.scummvm.scummvm", "net.namedfork.minivmac", "com.dry05.filzaescaped11-12", "com.virtualapplications.play"]
+    },
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "https://theodyssey.dev/altstore/odysseysource.json"},
+        "ids": ["org.coolstar.odyssey"]
+    },
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "https://taurine.app/altstore/taurinestore.json"},
+        "ids": ["com.odysseyteam.taurine"]
+    },
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "https://alt.getutm.app"},
+        "ids": ["com.utmapp.UTM", "com.utmapp.UTM-SE"]
+    },
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "https://demo.altstore.io"},
+        "ids": ["com.rileytestut.GBA4iOS"]
+    },
+    {
+        "parser": GithubParser,
+        "kwargs": {"repo_author": "iNDS-Team", "repo_name": "iNDS"},
+        "ids": ["net.nerd.iNDS"]
+    },
+    {
+        "parser": GithubParser,
+        "kwargs": {"repo_author": "yoshisuga", "repo_name": "MAME4iOS"},
+        "ids": ["com.example.mame4ios"]
+    },
+    {
+        "parser": GithubParser,
+        "kwargs": {"repo_author": "Provenance-Emu", "repo_name": "Provenance"},
+        "ids": ["com.provenance-emu.provenance"]
+    },
+    {
+        "parser": GithubParser,
+        "kwargs": {"repo_author": "brandonplank", "repo_name": "flappybird"},
+        "ids": ["org.brandonplank.flappybird15"]
+    },
+    {
+        "parser": GithubParser,
+        "kwargs": {"repo_author": "Wh0ba", "repo_name": "XPatcher"},
+        "ids": ["com.wh0ba.xpatcher"]
+    },
+    {
+        "parser": GithubParser,
+        "kwargs": {"repo_author": "litchie", "repo_name": "dospad"},
+        "ids": ["com.litchie.idosgames"]
+    },
+    {
+        "parser": GithubParser,
+        "kwargs": {"repo_author": "mariolopjr", "repo_name": "ppsspp-builder"},
+        "ids": ["org.ppsspp.ppsspp"]
+    }
+]
+alternateAppData = {
+    "org.ppsspp.ppsspp": {
+        "tintColor": "#21486b",
+        "subtitle": "PlayStation Portable games on iOS.",
+        "screenshotURLs": [
+            "https://i.imgur.com/CWl6GgH.png",
+            "https://i.imgur.com/SxmN1M0.png",
+            "https://i.imgur.com/sGWgR6z.png",
+            "https://i.imgur.com/AFKTdmZ.png"
+        ],
+        "iconURL": "https://i.imgur.com/JP0Fncv.png"
+    },
+    "com.rileytestut.GBA4iOS": {
+        "iconURL": "https://i.imgur.com/SBrqO9g.png",
+        "screenshotURLs": [
+            "https://i.imgur.com/L4H0yM3.png",
+            "https://i.imgur.com/UPGYLVr.png",
+            "https://i.imgur.com/sWpUAii.png",
+            "https://i.imgur.com/UwnDXRc.png"
+          ]
+    }
+}
+
+quantumsrc = AltSourceManager("quantumsource.json", sourcesData, alternateAppData, prettify=False) # if prettify is true, output will have indents and newlines
+try:
+    quantumsrc.update()
+except Exception as err:
+    print("Unable to update Quantum Source.")
+    print(f"{type(err).__name__}: {str(err)}")
+
+sourcesData = [
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "https://apps.altstore.io"},
+        "ids": ["com.rileytestut.AltStore", "com.rileytestut.AltStore.Beta", "com.rileytestut.Delta", "com.rileytestut.Delta.Beta", "com.rileytestut.Clip", "com.rileytestut.Clip.Beta"],
+        "getAllNews": True
+    },
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "https://alpha.altstore.io"},
+        "ids": ["com.rileytestut.AltStore.Alpha", "com.rileytestut.Delta.Alpha"],
+        "getAllNews": True
+    }
+]
+alternateAppData = {
+    "com.rileytestut.AltStore.Beta": {
+        "name": "AltStore (Beta)",
+        "beta": False
+    },
+    "com.rileytestut.Delta.Beta": {
+        "name": "Delta (Beta)",
+        "beta": False
+    },
+    "com.rileytestut.Clip.Beta": {
+        "name": "Clip (Beta)",
+        "beta": False
+    }
+}
+
+alt_complete = AltSourceManager("altstore-complete.json", sourcesData, alternateAppData, prettify=False)
+try:
+    alt_complete.update()
+except Exception as err:
+    print("Unable to update AltStore Complete.")
+    print(f"{type(err).__name__}: {str(err)}")
+
+sourcesData = [
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "quarksource++.json"},
+        "ids": ["com.google.ios.youtube", "com.crunchyroll.iphone", "com.duolingo.DuolingoMobile", "com.deezer.Deezer", "com.spotify.client", "com.atebits.Tweetie2", "syto203.reddit.pp", "com.antique.Popcorn-iOS", "mediaboxhd.event.2", "comicreader.net.ios", "com.channelsapp.channels", "com.burbn.instagram", "com.Lema.Michael.InstagramApp", "net.whatsapp.WhatsApp", "com.hotdog.popcorntime81.ios", "tv.twitch"]
+    },
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "quarksource-cracked.json"},
+        "ids": ["com.grailr.CARROTweather", "com.wolframalpha.wolframalpha", "com.firecore.infuse", "com.stey", "com.luma-touch.LumaFusion"]
+    },
+    {
+        "parser": AltSourceParser,
+        "kwargs": {"filepath": "https://9ani.app/api/altstore"},
+        "ids": ["com.marcuszhou.NineAnimator"]
+    },
+    {
+        "parser": GithubParser,
+        "kwargs": {"repo_author": "Paperback-iOS", "repo_name": "app", "ver_parse": lambda x: x.replace("-r", ".").lstrip("v")}, #ver_parse allows you to specify how to github tag should be processed before comparing to AltStore version tags
+        "ids": ["com.faizandurrani.paperback.ios"]
+    },
+    {
+        "parser": GithubParser,
+        "kwargs": {"repo_author": "tatsuz0u", "repo_name": "EhPanda"},
+        "ids": ["zeng.EhPanda"]
+    }
+]
+
+quantumsrc_plus = AltSourceManager("quantumsource++.json", sourcesData, prettify=False)
+try:
+    quantumsrc_plus.update()
+except Exception as err:
+    print("Unable to update Quantum Source Plus.")
+    print(f"{type(err).__name__}: {str(err)}")
